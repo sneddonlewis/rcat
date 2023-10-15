521ba9c2c40b053e28597bb51c3d1ee020f7ce92
lewis sneddon <lewis@pop-os.localdomain>
2023-10-15 08:51:58 +0100

    parsing args with clap

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,47 @@
 use std::error::Error;
 
+use clap::{App, Arg};
+
 pub type RcatResult<T> = Result<T, Box<dyn Error>>;
 
-pub fn run() -> RcatResult<()> {
-    println!("Hi there");
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    number_lines: bool,
+    number_nonblank_lines: bool,
+}
 
+pub fn get_config() -> RcatResult<Config> {
+    let matches = App::new("rcat")
+        .version("0.1.0")
+        .author("Lewis Sneddon")
+        .about("A Rusty Unix Cat, or a clone of the cat Unix tool to Rust")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .required(true)
+                .min_values(1)
+        )
+        .arg(
+            Arg::with_name("number_lines")
+                .short("n")
+                .takes_value(false)
+        )
+        .arg(
+            Arg::with_name("number_nonblank_lines")
+                .short("b")
+                .takes_value(false)
+        )
+        .get_matches();
+
+    Ok(Config { 
+        files: matches.values_of_lossy("files").unwrap(),
+        number_lines: matches.is_present("number_lines"),
+        number_nonblank_lines: matches.is_present("number_nonblank_lines"),
+    })
+}
+
+pub fn run(config: Config) -> RcatResult<()> {
+    dbg!(config);
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
-use rcat::run;
+use rcat::{run, get_config};
 
 fn main() {
-    if let Err(e) = run() {
-        eprintln!("{}", e);
-        std::process::exit(1);
+    let config_result = get_config();
+    match config_result {
+        Err(e) => {
+            eprintln!("{}", e);
+            std::process::exit(1);
+        },
+        Ok(config) => {
+            if let Err(e) = run(config) {
+                eprintln!("{}", e);
+                std::process::exit(1);
+            }
+        }
     }
 }
