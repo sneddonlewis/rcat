e954da53241642d172051beb2c64d558252c3485
lewis sneddon <lewis@pop-os.localdomain>
2023-10-15 10:16:19 +0100

    uncouth but works

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, fs::{self, File}, io::{BufRead, self, BufReader}};
 
 use clap::{App, Arg};
 
@@ -20,6 +20,7 @@ pub fn get_config() -> RcatResult<Config> {
             Arg::with_name("files")
                 .value_name("FILE")
                 .help("Input file(s)")
+                .multiple(true)
                 .default_value("-")
         )
         .arg(
@@ -28,10 +29,12 @@ pub fn get_config() -> RcatResult<Config> {
                 .long("number")
                 .help("Number lines")
                 .takes_value(false)
+                .conflicts_with("number_nonblank_lines")
         )
         .arg(
             Arg::with_name("number_nonblank_lines")
                 .short("b")
+                .long("number-nonblank")
                 .help("Number nonblank lines")
                 .takes_value(false)
         )
@@ -45,6 +48,55 @@ pub fn get_config() -> RcatResult<Config> {
 }
 
 pub fn run(config: Config) -> RcatResult<()> {
-    dbg!(config);
+    // read a file(files) line by line and print to STDIN
+    if config.number_lines {
+        for line in number_lines(read_lines(config.files)) {
+            println!("{}", line);
+        }
+    } else if config.number_nonblank_lines {
+        for line in number_nonblank_lines(read_lines(config.files)) {
+            println!("{}", line);
+        }
+    } else {
+        for line in read_lines(config.files) {
+            println!("{}", line);
+        }
+    }
+
     Ok(())
+}
+
+fn open(filename: &str) -> RcatResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+fn number_nonblank_lines(lines: Vec<String>) -> Vec<String> {
+    lines.iter().enumerate().map(|(idx, line)| {
+        if line.is_empty() {
+            String::from(line)
+        } else {
+            format!("{}\t{}", idx + 1, line)
+        }
+    }).collect()
+}
+
+fn number_lines(lines: Vec<String>) -> Vec<String> {
+    lines.iter().enumerate().map(|(idx, line)| {
+        format!("{}\t{}", idx + 1, line)
+    }).collect()
+}
+
+fn read_lines(files: Vec<String>) -> Vec<String> {
+    let mut result = Vec::new();
+
+    for file in &files {
+        for line in fs::read_to_string(file).unwrap().lines() {
+            result.push(String::from(line));
+        }
+    }
+
+    result
 }
